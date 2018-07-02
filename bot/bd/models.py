7ed6a4f3029b472f801d0a93bddb0fe86df03cd4
mrkotee <ezj08@ya.ru>
2018-07-02 14:36:43 +0300

    datetime import

--- bot/bd/models.py
+++ bot/bd/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
+from datetime import datetime as dt
 import os
 
 
